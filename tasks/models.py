c246a9411eb661f3cde70c4df59239d239ce10a4
Dmytro Shulha <dimonchik0218@gmail.com>
2026-06-14 19:21:31 +0300

    add to models fields blank, null, unique & create superuse

--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class TaskType(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
@@ -18,8 +18,8 @@ class PriorityChoices(models.TextChoices):
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
     deadline = models.DateTimeField()
     is_completed = models.BooleanField(default=False)
     priority = models.CharField(
@@ -42,7 +42,10 @@ class Task(models.Model):
 
 
 class Position(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
 
     def __str__(self):
         return self.name
@@ -52,6 +55,8 @@ class Worker(AbstractUser):
     position = models.ForeignKey(
         Position,
         on_delete=models.CASCADE,
+        null=True,
+        blank=True,
         related_name="workers",
     )
 
