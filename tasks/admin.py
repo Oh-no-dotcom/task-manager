a93f9b9c69994bf7268f0e47b19090c24804248c
Dmytro Shulha <dimonchik0218@gmail.com>
2026-06-14 18:27:36 +0300

    register Task with search_field & list_filter, TaskType, Position in admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from tasks.models import Worker
+from tasks.models import Worker, Task, TaskType, Position
 
 
 @admin.register(Worker)
 class WorkerAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("position",)
     fieldsets = UserAdmin.fieldsets + (
-        (("Additional Information", {"fields": ("position",)}),),
+        (("Additional information", {"fields": ("position",)}),),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
         (
-            "Additional Information",
+            "Additional information",
             {
                 "fields": (
                     "first_name",
@@ -24,3 +24,13 @@ class WorkerAdmin(UserAdmin):
     )
 
 
+@admin.register(Task)
+class TaskAdmin(UserAdmin):
+    search_fields = ("name", )
+    list_filter = ("task_type",)
+
+
+admin.site.register(TaskType)
+
+
+admin.site.register(Position)
