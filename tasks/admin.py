acbbfb9e94dfeda9f5a3a7f54429154d7b2130bf
Dmytro Shulha <dimonchik0218@gmail.com>
2026-06-14 18:20:08 +0300

    register Worker with addtional info in admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tasks.models import Worker
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional Information", {"fields": ("position",)}),),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional Information",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "position",
+                ),
+            }
+        )
+    )
+
+
