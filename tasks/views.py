fd2eea1be761c0319d9aaa062a5e24fca43a0c7b
Dmytro Shulha <dimonchik0218@gmail.com>
2026-06-14 20:22:34 +0300

    add path to urls.py in tasks&tasks_manager, created: view index, dir tasks in templates, empty index.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-# Create your views here.
+from tasks.models import Task
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+    num_tasks = Task.objects.all().count()
+    num_workers = User.objects.all().count()
+
+    context = {
+        "num_tasks": num_tasks,
+        "num_workers": num_workers,
+    }
+    return render(
+        request,
+        "tasks/index.html",
+        context=context
+    )
