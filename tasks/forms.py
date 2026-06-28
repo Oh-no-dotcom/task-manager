150da501d53a53f97d32b0da42b994d8b00f3608
Dmytro Shulha <dimonchik0218@gmail.com>
2026-06-28 23:45:39 +0300

    add WorkerSearchForm, get_queryset & get_context_data in view, add button search in worker_list

--- tasks/forms.py
+++ tasks/forms.py
@@ -127,3 +127,17 @@ class TaskSearchForm(forms.Form):
             }
         )
     )
+
+
+class WorkerSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by username",
+                "class": "form-control",
+            }
+        )
+    )
--- tasks/views.py
+++ tasks/views.py
@@ -13,6 +13,7 @@ from tasks.forms import (
     TaskTypeCreateForm,
     PositionCreateForm,
     TaskSearchForm,
+    WorkerSearchForm,
 )
 from tasks.models import (
     Task,
@@ -108,6 +109,30 @@ class TaskDetailView(LoginRequiredMixin, generic.DetailView):
 class WorkerListView(LoginRequiredMixin, generic.ListView):
     model = Worker
     paginate_by = 5
+
+    def get_context_data(
+            self,
+            *,
+            object_list=None,
+            **kwargs
+    ):
+        context = super(
+            WorkerListView,
+            self
+        ).get_context_data(**kwargs)
+        username = self.request.GET.get("username", "")
+        context["search_form"] = WorkerSearchForm(
+            initial={"username": username}
+        )
+        return context
+
+
+    def get_queryset(self):
+        queryset = Worker.objects.all()
+        form = WorkerSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(username__icontains=form.cleaned_data["username"])
+        return queryset
 
 
 class WorkerCreateView(LoginRequiredMixin, generic.CreateView):
