1f9e2d6f7737105f88e95557a104cb8475102e87
Dmytro Shulha <dimonchik0218@gmail.com>
2026-07-05 15:07:04 +0300

    add test for str methods of models

---
+++ tasks/tests/test_models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.utils import timezone
+
+from tasks.models import Task, TaskType, Position
+
+
+class ModelsTest(TestCase):
+    def test_task_str(self):
+        task_type = TaskType.objects.create(name="Bug")
+        task = Task.objects.create(
+            name="test",
+            deadline=timezone.now(),
+            task_type=task_type
+        )
+        self.assertEqual(str(task), "test")
+
+    def test_position_str(self):
+        position = Position.objects.create(name="Developer")
+
+        self.assertEqual(str(position), "Developer")
+
+    def test_task_type_str(self):
+        task_type = TaskType.objects.create(name="Bug")
+
+        self.assertEqual(str(task_type), "Bug")
+
+    def test_worker_str(self):
+        position = Position.objects.create(name="Developer")
+
+        worker = get_user_model().objects.create_user(
+            username="john_s",
+            password="test123",
+            position=position,
+            first_name="John",
+            last_name="Smith",
+        )
+        self.assertEqual(
+            str(worker),
+            f"{position} - {worker.username}"
+            f"({worker.first_name} {worker.last_name})"
+        )
