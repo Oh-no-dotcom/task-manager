007b6fee787db29b2f0e0b719f19cc74cd13a79e
Dmytro Shulha <dimonchik0218@gmail.com>
2026-07-05 14:32:37 +0300

    add TaskSearchFormTest

--- tasks/tests/test_forms.py
+++ tasks/tests/test_forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.utils import timezone
 
-from tasks.forms import WorkerCreationForm, TaskCreationForm
+from tasks.forms import WorkerCreationForm, TaskCreationForm, TaskSearchForm
 from tasks.models import Position, TaskType
 
 
@@ -49,3 +49,14 @@ class TaskCreationFormTest(TestCase):
 
         form = TaskCreationForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+
+class TaskSearchFormTest(TestCase):
+    def test_search_form_is_valid(self):
+        form = TaskSearchForm(data={"name": "test"})
+        self.assertTrue(form.is_valid())
+
+    def test_search_form_empty_is_valid(self):
+        form = TaskSearchForm(data={})
+        self.assertTrue(form.is_valid())
+
