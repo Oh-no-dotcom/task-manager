f7ea6a11d76617361a2d1553ca1dfb55259730c9
Dmytro Shulha <dimonchik0218@gmail.com>
2026-07-12 10:58:02 +0300

    prep for deploy, create build.sh update security secret_key

--- task_manager/settings/base.py
+++ task_manager/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-)1!-4c&(%2ju9&xum71h_4$y1!))@w$3xw3!7fdbnjalq*bv^-")
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 ASSETS_ROOT = "/static/assets"
 
